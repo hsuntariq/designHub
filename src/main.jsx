ab13a1a46e211418d34ed5a475dde46f1b02f029
unknown <hsuntariq@gmail.com>
2025-10-01 17:45:31 +0500

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { AppProvider } from "./context/context.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <AppProvider>
-    <App />
-  </AppProvider>
-);
+createRoot(document.getElementById("root")).render(<App />);
